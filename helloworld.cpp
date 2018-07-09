d96545096f715b8de319d23d22f2011d00d82046
serli09 <40759065+serli09@users.noreply.github.com>
2018-07-09 16:15:19 +0700

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //untuk menggunakan perintah cin,cout,endl
+#include <conio.h>//digunakan untuk (getch)
+using namespace std; //untuk pendeklarasian
+
+int main()
+{
+	cout<<"Hello World !!!"; //cout untuk menampilkan data
+	getch(); //untuk menahan tampilan
+}
